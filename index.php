710c5459827e8d494af13fe9ca7696404167f618
Daphne-Pham <phamxh22@juniata.edu>
2024-11-05 10:58:22 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+  <?php 
+echo('<h1>Welcome to our cool site</h1>'); 
+echo('<hr>');
+$speaktome = "IT 105 Programming"; 
+echo('<h3>' . $speaktome . '</h3>'); 
+?> 
+</body>
+</html>
